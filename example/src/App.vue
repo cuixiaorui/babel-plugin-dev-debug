d2e6852db6a9c4a129563ea6c69f0a4e10146620
cuixiaorui <cui_xiaorui@126.com>
2021-01-12 17:01:42 +0800

    add example

---
+++ example/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    example
+    <button @click="handleClick">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  setup() {
+    const handleClick = () => {
+      console.log("click");
+      if (DEBUG) {
+        console.log("for debug")
+        const a = 10;
+        const b = 20;
+        console.log(a + b);
+      }
+    };
+
+    return {
+      handleClick,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
